6fd5f53f5240451010dac97f45bd9e699e7537ad
freelancing-solutions <mobiusndou@gmail.com>
2023-02-26 16:50:54 +0200

    cached dynamic urls

--- src/prefetch/dynamic_urls.py
+++ src/prefetch/dynamic_urls.py
@@ -1,8 +1,15 @@
 # Prefetch endpoints
+import asyncio
 from itertools import chain
 
-from src.main.main import api_server_urls
+from src.config import config_instance
+from src.prefetch.exchange_lists import cached_exchange_lists
 from src.requests import requester
+from src.views_cache.cache import cached_ttl
+
+ONE_DAY = 60 * 60 * 24
+
+api_server_urls = [config_instance().API_SERVERS.MASTER_API_SERVER, config_instance().API_SERVERS.SLAVE_API_SERVER]
 
 PREFETCH_ENDPOINTS = [
     '/api/v1/exchanges',
@@ -13,11 +20,14 @@ PREFETCH_ENDPOINTS = [
 async def get_exchange_lists():
     server_url = api_server_urls[0]
     api_url = f"{server_url}/api/v1/exchanges"
-    response = await requester(api_url=api_url)
-    data = response.json()
-    if data.get("status"):
-        return data.get("payload")
-    return {}
+    try:
+        _response = await requester(api_url=api_url)
+        data = _response.json()
+        if data.get("status"):
+            return data.get("payload")
+    except Exception as e:
+        pass
+    return cached_exchange_lists
 
 
 async def get_exchange_codes():
@@ -36,26 +46,37 @@ async def get_countries():
     return [exchange.get("country") for exchange in await get_exchange_lists()]
 
 
+@cached_ttl(ttl=ONE_DAY)
 async def build_dynamic_urls() -> list[str]:
     """
         dynamic urls to prefetch
     :return:
     """
-    codes_urls = ['/api/v1/exchange/listed-stocks/',
-                  '/api/v1/stocks/exchange/code/']
+    codes_urls = ['/api/v1/exchange/listed-stocks/', '/api/v1/stocks/exchange/code/']
     exchange_by_id_url = "/api/v1/stocks/exchange/id/"
     stocks_by_currency_url = "/api/v1/stocks/currency/"
     stocks_by_country_url = "/api/v1/stocks/country/"
+
     expanded_urls = []
     for server_url in api_server_urls:
         for endpoint in codes_urls:
             for code in await get_exchange_codes():
-                expanded_urls.append(f"{server_url}{endpoint}{code}")
+                if code and code is not "null":
+                    expanded_urls.append(f"{server_url}{endpoint}{code}")
         for _id in await get_exchange_ids():
-            expanded_urls.append(f"{server_url}{exchange_by_id_url}{_id}")
+            if _id and _id is not "null":
+                expanded_urls.append(f"{server_url}{exchange_by_id_url}{_id}")
         for _currency in await get_currencies():
-            expanded_urls.append(f"{server_url}{stocks_by_currency_url}{_currency}")
+            if _currency and _currency is not "null":
+                expanded_urls.append(f"{server_url}{stocks_by_currency_url}{_currency}")
         for _country in await get_countries():
-            expanded_urls.append(f"{server_url}{stocks_by_country_url}{_country}")
-    urls = list(chain(*[expanded_urls, PREFETCH_ENDPOINTS]))
-    return urls
+            if _country and _country is not "null":
+                expanded_urls.append(f"{server_url}{stocks_by_country_url}{_country}")
+
+    return list(chain(*[expanded_urls, PREFETCH_ENDPOINTS]))
+
+
+if __name__ == "__main__":
+    response = asyncio.run(build_dynamic_urls())
+    print(response)
+    print(f"total urls: {len(response)} ")
---
+++ src/prefetch/exchange_lists.py
@@ -0,0 +1,594 @@
+cached_exchange_lists = [
+        {
+            "code": "BA",
+            "country": "Argentina",
+            "currency_symbol": "ARS",
+            "exchange_id": "RP5zfbmYel63f0Wc",
+            "name": "Buenos Aires Exchange",
+            "operating_mic": "XBUE"
+        },
+        {
+            "code": "AU",
+            "country": "Australia",
+            "currency_symbol": "AUD",
+            "exchange_id": "Tl2hlbpejIQflm94",
+            "name": "Australia Exchange",
+            "operating_mic": "XASX"
+        },
+        {
+            "code": "VI",
+            "country": "Austria",
+            "currency_symbol": "EUR",
+            "exchange_id": "LmSa3cscWaSPXc5D",
+            "name": "Vienna Exchange",
+            "operating_mic": "XWBO"
+        },
+        {
+            "code": "EUBOND",
+            "country": "Belgium",
+            "currency_symbol": "EUR",
+            "exchange_id": "xKY5VqCL2dK1wXZL",
+            "name": "EU Bond Virtual Exchange",
+            "operating_mic": None
+        },
+        {
+            "code": "BR",
+            "country": "Belgium",
+            "currency_symbol": "EUR",
+            "exchange_id": "66ksM2wHta7mRLns",
+            "name": "Euronext Brussels",
+            "operating_mic": "XBRU"
+        },
+        {
+            "code": "SA",
+            "country": "Brazil",
+            "currency_symbol": "BRL",
+            "exchange_id": "nlRDVmQ7YjRROqjO",
+            "name": "Sao Paolo Exchange",
+            "operating_mic": "BVMF"
+        },
+        {
+            "code": "NEO",
+            "country": "Canada",
+            "currency_symbol": "CAD",
+            "exchange_id": "lA9HtGfHm3RxKWV0",
+            "name": "NEO Exchange",
+            "operating_mic": "NEOE"
+        },
+        {
+            "code": "TO",
+            "country": "Canada",
+            "currency_symbol": "CAD",
+            "exchange_id": "WPqMzJHtpJnPmB0E",
+            "name": "Toronto Exchange",
+            "operating_mic": "XTSE"
+        },
+        {
+            "code": "V",
+            "country": "Canada",
+            "currency_symbol": "CAD",
+            "exchange_id": "vyHhUtDiKF2TL0vh",
+            "name": "TSX Venture Exchange",
+            "operating_mic": "XTSX"
+        },
+        {
+            "code": "SN",
+            "country": "Chile",
+            "currency_symbol": "CLP",
+            "exchange_id": "90pZEGoHenFJlJvr",
+            "name": "Chilean Stock Exchange",
+            "operating_mic": "XSGO"
+        },
+        {
+            "code": "SHG",
+            "country": "China",
+            "currency_symbol": "CNY",
+            "exchange_id": "4xeDYPg8WTJ8q5cp",
+            "name": "Shanghai Exchange",
+            "operating_mic": "XSHG"
+        },
+        {
+            "code": "SHE",
+            "country": "China",
+            "currency_symbol": "CNY",
+            "exchange_id": "PTXIlhGNdSIAJH8A",
+            "name": "Shenzhen Exchange",
+            "operating_mic": "XSHE"
+        },
+        {
+            "code": "ZSE",
+            "country": "Croatia",
+            "currency_symbol": "EUR",
+            "exchange_id": "icBmpIzojb2ZESPe",
+            "name": "Zagreb Stock Exchange",
+            "operating_mic": "XZAG"
+        },
+        {
+            "code": "PR",
+            "country": "Czech Republic",
+            "currency_symbol": "CZK",
+            "exchange_id": "NCQ4KhNZBGViIHjY",
+            "name": "Prague Stock Exchange ",
+            "operating_mic": "XPRA"
+        },
+        {
+            "code": "CO",
+            "country": "Denmark",
+            "currency_symbol": "DKK",
+            "exchange_id": "wSQc1tUJUeTA3rFd",
+            "name": "Copenhagen Exchange",
+            "operating_mic": "XCSE"
+        },
+        {
+            "code": "HE",
+            "country": "Finland",
+            "currency_symbol": "EUR",
+            "exchange_id": "qk5mADFnav3OQje6",
+            "name": "Helsinki Exchange",
+            "operating_mic": "XHEL"
+        },
+        {
+            "code": "PA",
+            "country": "France",
+            "currency_symbol": "EUR",
+            "exchange_id": "0j77SEouifcXFUj9",
+            "name": "Euronext Paris",
+            "operating_mic": "XPAR"
+        },
+        {
+            "code": "BE",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "Io4WbG2POC03xoou",
+            "name": "Berlin Exchange",
+            "operating_mic": "XBER"
+        },
+        {
+            "code": "DU",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "Gl8XvD7jzjpvrOiz",
+            "name": "Dusseldorf Exchange",
+            "operating_mic": "XDUS"
+        },
+        {
+            "code": "F",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "5flEgKq6zF3egBJ4",
+            "name": "Frankfurt Exchange",
+            "operating_mic": "XFRA"
+        },
+        {
+            "code": "HM",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "DIr6XmO7Ekgf3xZY",
+            "name": "Hamburg Exchange",
+            "operating_mic": "XHAM"
+        },
+        {
+            "code": "HA",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "QbUfxddkuAf92BpV",
+            "name": "Hanover Exchange",
+            "operating_mic": "XHAN"
+        },
+        {
+            "code": "MU",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "RMO9rrinzSNfGoTh",
+            "name": "Munich Exchange",
+            "operating_mic": "XMUN"
+        },
+        {
+            "code": "STU",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "R1KaRomYKNDZw7kF",
+            "name": "Stuttgart Exchange",
+            "operating_mic": "XSTU"
+        },
+        {
+            "code": "XETRA",
+            "country": "Germany",
+            "currency_symbol": "EUR",
+            "exchange_id": "5mVxUUxl17lXkbxb",
+            "name": "XETRA Exchange",
+            "operating_mic": "XETR"
+        },
+        {
+            "code": "AT",
+            "country": "Greece",
+            "currency_symbol": "EUR",
+            "exchange_id": "7ilTcz0lXtMSqIxV",
+            "name": "Athens Exchange",
+            "operating_mic": "ASEX"
+        },
+        {
+            "code": "HK",
+            "country": "Hong Kong",
+            "currency_symbol": "HKD",
+            "exchange_id": "ecUnBkCMNlCHNFBV",
+            "name": "Hong Kong Exchange",
+            "operating_mic": "XHKG"
+        },
+        {
+            "code": "BUD",
+            "country": "Hungary",
+            "currency_symbol": "HUF",
+            "exchange_id": "myhJmJd4X9k8PIz1",
+            "name": "Budapest Stock Exchange",
+            "operating_mic": "XBUD"
+        },
+        {
+            "code": "IC",
+            "country": "Iceland",
+            "currency_symbol": "ISK",
+            "exchange_id": "4l69uLTntejXBANf",
+            "name": "Iceland Exchange",
+            "operating_mic": "XICE"
+        },
+        {
+            "code": "BSE",
+            "country": "India",
+            "currency_symbol": "INR",
+            "exchange_id": "GrGvKxrQJcEb4NJE",
+            "name": "Bombay Exchange",
+            "operating_mic": "XBOM"
+        },
+        {
+            "code": "NSE",
+            "country": "India",
+            "currency_symbol": "INR",
+            "exchange_id": "Nndl7maA17tghYFN",
+            "name": "NSE (India)",
+            "operating_mic": "XNSE"
+        },
+        {
+            "code": "JK",
+            "country": "Indonesia",
+            "currency_symbol": "IDR",
+            "exchange_id": "CB9M04p16gieLuh9",
+            "name": "Jakarta Exchange",
+            "operating_mic": "XIDX"
+        },
+        {
+            "code": "IR",
+            "country": "Ireland",
+            "currency_symbol": "EUR",
+            "exchange_id": "nOO6TIsc9iuepmTE",
+            "name": "Irish Exchange",
+            "operating_mic": "XDUB"
+        },
+        {
+            "code": "TA",
+            "country": "Israel",
+            "currency_symbol": "ILS",
+            "exchange_id": "wKuqcEkBZXHbhzQC",
+            "name": "Tel Aviv Exchange",
+            "operating_mic": "XTAE"
+        },
+        {
+            "code": "MI",
+            "country": "Italy",
+            "currency_symbol": "EUR",
+            "exchange_id": "b0Ryno5rQcjNgi1U",
+            "name": "Borsa Italiana",
+            "operating_mic": "XMIL"
+        },
+        {
+            "code": "TSE",
+            "country": "Japan",
+            "currency_symbol": "JPY",
+            "exchange_id": "joY1CTw7wNpZqJZA",
+            "name": "Tokyo Stock Exchange",
+            "operating_mic": "XJPX"
+        },
+        {
+            "code": "KO",
+            "country": "Korea",
+            "currency_symbol": "KRW",
+            "exchange_id": "S1Uq7006jpHHEulx",
+            "name": "Korea Stock Exchange",
+            "operating_mic": "XKRX"
+        },
+        {
+            "code": "KQ",
+            "country": "Korea",
+            "currency_symbol": "KRW",
+            "exchange_id": "VUlcbNCCPBKJvSDd",
+            "name": "KOSDAQ",
+            "operating_mic": "XKOS"
+        },
+        {
+            "code": "LU",
+            "country": "Luxembourg",
+            "currency_symbol": "EUR",
+            "exchange_id": "qbBwqsXbfdZWlYcJ",
+            "name": "Luxembourg Stock Exchange",
+            "operating_mic": "XLUX"
+        },
+        {
+            "code": "KLSE",
+            "country": "Malaysia",
+            "currency_symbol": "MYR",
+            "exchange_id": "cysTFUbV4eVChZ5n",
+            "name": "Kuala Lumpur Exchange",
+            "operating_mic": "XKLS"
+        },
+        {
+            "code": "MX",
+            "country": "Mexico",
+            "currency_symbol": "MXN",
+            "exchange_id": "nM5SCa8VZCoB7cYE",
+            "name": "Mexican Exchange",
+            "operating_mic": "XMEX"
+        },
+        {
+            "code": "AS",
+            "country": "Netherlands",
+            "currency_symbol": "EUR",
+            "exchange_id": "6UfIHcQ6EhE4NuPj",
+            "name": "Euronext Amsterdam",
+            "operating_mic": "XAMS"
+        },
+        {
+            "code": "OL",
+            "country": "Norway",
+            "currency_symbol": "NOK",
+            "exchange_id": "x6wOQHrT58ohmibr",
+            "name": "Oslo Stock Exchange",
+            "operating_mic": "XOSL"
+        },
+        {
+            "code": "KAR",
+            "country": "Pakistan",
+            "currency_symbol": "PKR",
+            "exchange_id": "LMFO5J8uTfRt1VLI",
+            "name": "Karachi Stock Exchange",
+            "operating_mic": "XKAR"
+        },
+        {
+            "code": "LIM",
+            "country": "Peru",
+            "currency_symbol": "PEN",
+            "exchange_id": "FB1n7jvAFHAJ2RG0",
+            "name": "Bolsa de Valores de Lima",
+            "operating_mic": "XLIM"
+        },
+        {
+            "code": "PSE",
+            "country": "Philippines",
+            "currency_symbol": "PHP",
+            "exchange_id": "URSgoEqNq9pswChJ",
+            "name": "Philippine Stock Exchange",
+            "operating_mic": "XPHS"
+        },
+        {
+            "code": "WAR",
+            "country": "Poland",
+            "currency_symbol": "PLN",
+            "exchange_id": "EXIuJG0xGd8GpbfQ",
+            "name": "Warsaw Stock Exchange",
+            "operating_mic": "XWAR"
+        },
+        {
+            "code": "LS",
+            "country": "Portugal",
+            "currency_symbol": "EUR",
+            "exchange_id": "gid79gYd6paOENqr",
+            "name": "Euronext Lisbon",
+            "operating_mic": "XLIS"
+        },
+        {
+            "code": "RO",
+            "country": "Romania",
+            "currency_symbol": "RON",
+            "exchange_id": "DJCNqK9C7wB7RFjK",
+            "name": "Bucharest Stock Exchange",
+            "operating_mic": "XBSE"
+        },
+        {
+            "code": "MCX",
+            "country": "Russia",
+            "currency_symbol": "RUB",
+            "exchange_id": "ru26tlEiQsp0S14D",
+            "name": "MICEX Moscow Russia",
+            "operating_mic": None
+        },
+        {
+            "code": "SR",
+            "country": "Saudi Arabia",
+            "currency_symbol": "SAR",
+            "exchange_id": "AkPcNdmJ40JKV6Rn",
+            "name": "Saudi Arabia Exchange",
+            "operating_mic": "XSAU"
+        },
+        {
+            "code": "SG",
+            "country": "Singapore",
+            "currency_symbol": "SGD",
+            "exchange_id": "CfG57zw9lZDogBY2",
+            "name": "Singapore Exchange",
+            "operating_mic": "XSES"
+        },
+        {
+            "code": "JSE",
+            "country": "South Africa",
+            "currency_symbol": "ZAR",
+            "exchange_id": "zhwLLl0wvrrJCfyY",
+            "name": "Johannesburg Exchange",
+            "operating_mic": "XJSE"
+        },
+        {
+            "code": "MC",
+            "country": "Spain",
+            "currency_symbol": "EUR",
+            "exchange_id": "hneT6zETUilAivfj",
+            "name": "Madrid Exchange",
+            "operating_mic": "BMEX"
+        },
+        {
+            "code": "CM",
+            "country": "Sri Lanka",
+            "currency_symbol": "LKR",
+            "exchange_id": "CeTCDanBWfB16GjZ",
+            "name": "Colombo Stock Exchange",
+            "operating_mic": "XCOL"
+        },
+        {
+            "code": "ST",
+            "country": "Sweden",
+            "currency_symbol": "SEK",
+            "exchange_id": "veJjGjclr0a3r0OE",
+            "name": "Stockholm Exchange",
+            "operating_mic": "XSTO"
+        },
+        {
+            "code": "SW",
+            "country": "Switzerland",
+            "currency_symbol": "CHF",
+            "exchange_id": "8rCpRT5zSL7UIR66",
+            "name": "SIX Swiss Exchange",
+            "operating_mic": "XSWX"
+        },
+        {
+            "code": "VX",
+            "country": "Switzerland",
+            "currency_symbol": "CHF",
+            "exchange_id": "61sW1hTld40S0aQA",
+            "name": "Swiss Exchange",
+            "operating_mic": "XSWX"
+        },
+        {
+            "code": "TW",
+            "country": "Taiwan",
+            "currency_symbol": "TWD",
+            "exchange_id": "au36tlsrRl8p0XNM",
+            "name": "Taiwan Exchange",
+            "operating_mic": "XTAI"
+        },
+        {
+            "code": "TWO",
+            "country": "Taiwan",
+            "currency_symbol": "TWD",
+            "exchange_id": "NmiJeicMEkXqFByT",
+            "name": "Taiwan OTC Exchange",
+            "operating_mic": "ROCO"
+        },
+        {
+            "code": "BK",
+            "country": "Thailand",
+            "currency_symbol": "THB",
+            "exchange_id": "wvc1PRdacTkBLlL9",
+            "name": "Thailand Exchange",
+            "operating_mic": "XBKK"
+        },
+        {
+            "code": "IS",
+            "country": "Turkey",
+            "currency_symbol": "TRY",
+            "exchange_id": "YopMWgK4084lOAY4",
+            "name": "Istanbul Stock Exchange",
+            "operating_mic": "XIST"
+        },
+        {
+            "code": "LSE",
+            "country": "UK",
+            "currency_symbol": "GBP",
+            "exchange_id": "fl4Ov6jInhoqqlO7",
+            "name": "London Exchange",
+            "operating_mic": "XLON"
+        },
+        {
+            "code": "IL",
+            "country": "UK",
+            "currency_symbol": "USD",
+            "exchange_id": "9BF3jsJXJwE3FZ2f",
+            "name": "London IL",
+            "operating_mic": "XLON"
+        },
+        {
+            "code": "COMM",
+            "country": "Unknown",
+            "currency_symbol": None,
+            "exchange_id": "KGqDXitsl6U0QL7N",
+            "name": "Commodities",
+            "operating_mic": None
+        },
+        {
+            "code": "CC",
+            "country": "Unknown",
+            "currency_symbol": None,
+            "exchange_id": "k3Ao1615tIBsZdru",
+            "name": "Cryptocurrencies",
+            "operating_mic": "CRYP"
+        },
+        {
+            "code": "EUFUND",
+            "country": "Unknown",
+            "currency_symbol": "EUR",
+            "exchange_id": "fQmY5qXs5cSXDX3D",
+            "name": "Europe Fund Virtual Exchange",
+            "operating_mic": None
+        },
+        {
+            "code": "FOREX",
+            "country": "Unknown",
+            "currency_symbol": None,
+            "exchange_id": "17tcGWD3PrJ8BmWm",
+            "name": "FOREX",
+            "operating_mic": "CDSL"
+        },
+        {
+            "code": "GBOND",
+            "country": "Unknown",
+            "currency_symbol": None,
+            "exchange_id": "EIzkSIMhaRgg50c2",
+            "name": "Government Bonds",
+            "operating_mic": None
+        },
+        {
+            "code": "INDX",
+            "country": "Unknown",
+            "currency_symbol": None,
+            "exchange_id": "VHzYP0hiIKYzGNLC",
+            "name": "Indices",
+            "operating_mic": None
+        },
+        {
+            "code": "MONEY",
+            "country": "Unknown",
+            "currency_symbol": None,
+            "exchange_id": "qtDFxhoJlJ7hNL3L",
+            "name": "Money Market Virtual Exchange",
+            "operating_mic": None
+        },
+        {
+            "code": "BOND",
+            "country": "USA",
+            "currency_symbol": "USD",
+            "exchange_id": "wKA5GAyiDmSPpAWF",
+            "name": "Bond Virtual Exchange",
+            "operating_mic": None
+        },
+        {
+            "code": "US",
+            "country": "USA",
+            "currency_symbol": "USD",
+            "exchange_id": "SsnkrK09oZtKfwFk",
+            "name": "USA Stocks",
+            "operating_mic": "XNAS, XNYS"
+        },
+        {
+            "code": "VN",
+            "country": "Vietnam",
+            "currency_symbol": "VND",
+            "exchange_id": "1Ib7UatNwoCoTv8t",
+            "name": "Vietnam Stocks",
+            "operating_mic": "HSTC"
+        }
+    ]
