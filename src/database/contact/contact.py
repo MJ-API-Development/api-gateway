ab4f71d4e338b735dddff67c2b4d779d0be42de4
freelancing-solutions <mobiusndou@gmail.com>
2023-03-01 20:28:03 +0200

    fixed bug on contacts

--- src/database/contact/contact.py
+++ src/database/contact/contact.py
@@ -2,9 +2,8 @@
 from datetime import datetime
 from sqlalchemy import Column, String, inspect, Integer, Float, ForeignKey
 
-from src.databases.const import UUID_LEN, NAME_LEN, EMAIL_LEN, STR_LEN
-from src.databases.models.sql import Base, mysql_instance
-from src.utils import date_from_timestamp
+from src.const import UUID_LEN, NAME_LEN, EMAIL_LEN, STR_LEN
+from src.database.database_sessions import Base, engine
 
 
 class Contacts(Base):
@@ -12,19 +11,19 @@ class Contacts(Base):
         ORM Model for Contacts
     """
     __tablename__ = 'contact_messages'
-    uuid = Column(String(UUID_LEN), index=True, nullable=True)
-    contact_id = Column(String(UUID_LEN), primary_key=True, index=True)
-    name = Column(String(NAME_LEN), index=True)
-    email = Column(String(EMAIL_LEN), ForeignKey("accounts.email"), index=True)
-    message = Column(String(STR_LEN))
+    uuid: str = Column(String(UUID_LEN), index=True, nullable=True)
+    contact_id: str = Column(String(UUID_LEN), primary_key=True, index=True)
+    name: str = Column(String(NAME_LEN), index=True)
+    email: str = Column(String(EMAIL_LEN), ForeignKey("accounts.email"), index=True)
+    message: str = Column(String(STR_LEN))
     timestamp = Column(Float, index=True)
 
     @property
     def datetime(self) -> datetime:
-        return date_from_timestamp(self.timestamp)
+        return datetime.fromtimestamp(self.timestamp)
 
     @classmethod
     def create_if_not_exists(cls):
-        if not inspect(mysql_instance.engine).has_table(cls.__tablename__):
-            Base.metadata.create_all(bind=mysql_instance.engine)
+        if not inspect(engine).has_table(cls.__tablename__):
+            Base.metadata.create_all(bind=engine)
 
