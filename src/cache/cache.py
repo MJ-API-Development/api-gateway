58ab87b5736912dedea57c1dfc23980a16ddc043
freelancing-solutions <mobiusndou@gmail.com>
2023-03-05 09:50:00 +0200

    updated cache mechanism

--- src/cache/cache.py
+++ src/cache/cache.py
@@ -18,21 +18,17 @@ async def create_key(method: str, kwargs: dict[str, str | int]) -> str:
 def cached(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
-        new_kwargs = kwargs.copy()
-        for key, value in kwargs.items():
-            if key == 'session':
-                # removing session from keys
-                _ = new_kwargs.pop(key)
-        if args:
-            api_url, = args
-            new_kwargs.update(dict(api_url=api_url))
+
+        api_url, = args if len(args) == 1 else None
+        new_kwargs = {'api_url': api_url} if api_url is not None else {}
+        new_kwargs.update({k: v for k, v in kwargs.items() if k != 'session'})
         _key = await create_key(method=func.__name__, kwargs=new_kwargs)
 
-        _data = await mem_cache.get(_key)
+        _data = await redis_cache.get(_key)
         if _data is None:
             result = await func(*args, **kwargs)
             if result:
-                await mem_cache.set(key=_key, value=result)
+                await redis_cache.set(key=_key, value=result)
                 # redis_cache.set(key=_key, value=result, expiration_time=60*60*1)
             return result
         return _data
@@ -44,22 +40,16 @@ def cached_ttl(ttl: int = 60 * 60 * 1):
     def _cached(func):
         @functools.wraps(func)
         async def _wrapper(*args, **kwargs):
-            new_kwargs = kwargs.copy()
-            for key, value in kwargs.items():
-                if key == 'session':
-                    # removing session from keys
-                    _ = new_kwargs.pop(key)
-
-            if args:
-                api_url, = args
-                new_kwargs.update(dict(api_url=api_url))
-
+            api_url, = args if len(args) == 1 else None
+            new_kwargs = {'api_url': api_url} if api_url is not None else {}
+            new_kwargs.update({k: v for k, v in kwargs.items() if k != 'session'})
             _key = await create_key(method=func.__name__, kwargs=new_kwargs)
-            _data = await mem_cache.get(_key)
+
+            _data = await redis_cache.get(_key)
             if _data is None:
                 result = await func(*args, **kwargs)
                 if result:
-                    await mem_cache.set(key=_key, value=result)
+                    await redis_cache.set(key=_key, value=result, ttl=ttl)
                     # redis_cache.set(key=_key, value=result, expiration_time=60*60*1)
                 return result
             return _data
@@ -77,14 +67,9 @@ def redis_cached(func):
     """
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
-        new_kwargs = kwargs.copy()
-        for key, value in kwargs.items():
-            if key == 'session':
-                # removing session from keys
-                _ = new_kwargs.pop(key)
-        if args:
-            api_url, = args
-            new_kwargs.update(dict(api_url=api_url))
+        api_url, = args if len(args) == 1 else None
+        new_kwargs = {'api_url': api_url} if api_url is not None else {}
+        new_kwargs.update({k: v for k, v in kwargs.items() if k != 'session'})
         _key = await create_key(method=func.__name__, kwargs=new_kwargs)
 
         _data = await redis_cache.get(_key)
@@ -107,17 +92,12 @@ def redis_cached_ttl(ttl: int = 60 * 60 * 1):
     def _redis_cached(func):
         @functools.wraps(func)
         async def _wrapper(*args, **kwargs):
-            new_kwargs = kwargs.copy()
-            for key, value in kwargs.items():
-                if key == 'session':
-                    # removing session from keys
-                    _ = new_kwargs.pop(key)
 
-            if args:
-                api_url, = args
-                new_kwargs.update(dict(api_url=api_url))
-
+            api_url, = args if len(args) == 1 else None
+            new_kwargs = {'api_url': api_url} if api_url is not None else {}
+            new_kwargs.update({k: v for k, v in kwargs.items() if k != 'session'})
             _key = await create_key(method=func.__name__, kwargs=new_kwargs)
+
             _data = await redis_cache.get(_key)
             if _data is None:
                 result = await func(*args, **kwargs)
@@ -133,4 +113,4 @@ def redis_cached_ttl(ttl: int = 60 * 60 * 1):
 
 redis_cache = Cache(cache_name="redis", use_redis=True)
 asyncio.run(redis_cache.create_redis_pool())
-mem_cache = Cache(cache_name="mem_cache", use_redis=False)
+# mem_cache = Cache(cache_name="mem_cache", use_redis=False)
