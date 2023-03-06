8fc8676cb3f82766a386ffabfdecc8209c0810f3
freelancing-solutions <mobiusndou@gmail.com>
2023-03-06 08:39:39 +0200

    fixed routing bug preventing cloudflare gateway worker from working

--- src/requests/__init__.py
+++ src/requests/__init__.py
@@ -3,8 +3,17 @@ from src.config import config_instance
 
 # Use the connection pool limits in the AsyncClient
 
+_headers = {
+    'X-API-KEY': config_instance().API_SERVERS.X_API_KEY,
+    'X-SECRET-TOKEN': config_instance().API_SERVERS.X_SECRET_TOKEN,
+    'X-RapidAPI-Proxy-Secret': config_instance().API_SERVERS.X_RAPID_SECRET,
+    'Content-Type': "application/json",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+}
 
-async_client = httpx.AsyncClient(http2=True, limits=httpx.Limits(max_connections=100, max_keepalive_connections=20))
+async_client = httpx.AsyncClient(http2=True,
+                                 limits=httpx.Limits(max_connections=100, max_keepalive_connections=20),
+                                 headers=_headers)
 
 
 async def requester(api_url: str, timeout: int = 30):
@@ -15,8 +24,7 @@ async def requester(api_url: str, timeout: int = 30):
     :return:
     """
     try:
-        headers = await set_headers()
-        response = await async_client.get(url=api_url, headers=headers, timeout=timeout)
+        response = await async_client.get(url=api_url, timeout=timeout)
     except httpx.HTTPError as http_err:
         raise http_err
     except Exception as err:
@@ -32,5 +40,3 @@ async def set_headers():
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
 
 # TODO Try random election
-
-
