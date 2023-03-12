5145540a855482bc347c5e8d94ad20262148532e
freelancing-solutions <mobiusndou@gmail.com>
2023-03-12 10:15:06 +0200

    completed user management tasks

---
+++ src/management_api/models/users.py
@@ -0,0 +1,26 @@
+
+from typing import Optional
+from pydantic import BaseModel
+
+
+class AccountUpdate(BaseModel):
+    uuid: str
+    first_name: str | None = None
+    second_name: str | None = None
+    surname: str | None = None
+    email: str | None = None
+    cell: str | None = None
+    password_hash: str | None = None
+    is_admin: bool | None = None
+    is_deleted: bool | None = None
+
+
+class AccountCreate(BaseModel):
+    first_name: str
+    second_name: str | None = None
+    surname: str
+    email: str
+    cell: str
+    password: str
+    is_admin: bool | None = False
+    is_deleted: bool | None = False
--- src/management_api/routers/users/users.py
+++ src/management_api/routers/users/users.py
@@ -1,96 +1,117 @@
-
 from fastapi import APIRouter, Request, HTTPException
 from starlette.responses import JSONResponse
 
 from src.config import config_instance
+from src.const import UUID_LEN
 from src.database.account.account import Account
 from src.database.database_sessions import sessions
 from src.email.email import email_process
 from src.management_api.admin.authentication import authenticate_app, get_headers
+from src.management_api.models.users import AccountUpdate, AccountCreate
 
 from src.utils.my_logger import init_logger
+from src.utils.utils import create_id
 
 users_router = APIRouter()
 users_logger = init_logger("users_router")
 
 
-@users_router.api_route(path="/user", methods=["POST", "PUT"], include_in_schema=True)
+@users_router.api_route(path="/user", methods=["POST"], include_in_schema=True)
 @authenticate_app
-async def create_update_user(request: Request, user_data: dict[str, str | int | bool]):
+async def create_user(user_data: AccountCreate):
     """
         used to create new user record
     :param user_data:
-    :param request:
+
     :return:
     """
-    headers = {'Content-Type': 'application/json'}
+
     with next(sessions) as session:
-        if request.method == "POST":
-            users_logger.info("create user")
-            email = user_data.get("email")
-            user_instance = await Account.get_by_email(email=email, session=session)
-            if not user_instance:
-                user_instance = Account(**user_data)
-                session.add(user_instance)
-                account_dict = dict()
-                # this will schedule an account confirmation email to be sent
-                # TODO look at this - Make this an ephemeral link, it other words it should expire after sometime
-                verification_link = f"https://gateway.eod-stock-api.site/_admin/account/confirm/{account_dict.get('uuid')}"
+        users_logger.info(f"creating user with the following user data : {user_data}")
+        email = user_data.email
+        user_instance = await Account.get_by_email(email=email, session=session)
+        if not user_instance:
+            user_dict = user_data.dict()
+            user_dict.update(uuid=create_id(UUID_LEN))
+            user_instance = Account(**user_dict)
+            session.add(user_instance)
+            # this will schedule an account confirmation email to be sent
+            # TODO look at this - Make this an ephemeral link, it other words it should expire after sometime
+            verification_link = f"https://gateway.eod-stock-api.site/_admin/account/confirm/{user_dict.get('uuid')}"
 
-                sender_email = config_instance().EMAIL_SETTINGS.ADMIN
-                recipient_email = account_dict.get('email')
-                client_name = account_dict.get('name')
-                message_dict = dict(verification_link=verification_link, sender_email=sender_email,
-                                    recipient_email=recipient_email, client_name=client_name)
+            sender_email = config_instance().EMAIL_SETTINGS.ADMIN
+            recipient_email = user_instance.email
+            client_name = user_instance.names
+            message_dict = dict(verification_link=verification_link, sender_email=sender_email,
+                                recipient_email=recipient_email, client_name=client_name)
 
-                await email_process.send_account_confirmation_email(**message_dict)
-            else:
-                raise HTTPException(detail="User already exist", status_code=401)
+            await email_process.send_account_confirmation_email(**message_dict)
+            payload = dict(status=True, payload=user_instance.to_dict(), message="successfully created account")
+            _headers = await get_headers(user_data=payload)
+            return JSONResponse(content=payload, status_code=201, headers=_headers)
 
-        elif request.method == "PUT":
-            uuid = user_data.get('uuid')
-            user_instance = await Account.get_by_uuid(uuid=uuid, session=session)
-            user_instance = user_instance(**user_data)
-            session.merge(user_instance)
-
-        session.commit()
-        headers = await get_headers(user_data=user_instance.to_dict())
-        return JSONResponse(content=user_instance.to_dict(), status_code=201, headers=headers)
+        else:
+            # TODO create custom Exceptions
+            raise HTTPException(detail="User already exist", status_code=401)
 
 
-@users_router.api_route(path="/user/{path}", methods=["GET", "DELETE"], include_in_schema=True)
+@users_router.api_route(path="/user", methods=["PUT"], include_in_schema=True)
 @authenticate_app
-async def get_delete_user(request: Request, path: str):
+async def update_user(user_data: AccountUpdate):
+    """
+    **update_user**
+        given some fields on user_data update the user instance with those fields
+    """
+    with next(sessions) as session:
+        uuid = user_data.uuid
+        user_instance: Account = await Account.get_by_uuid(uuid=uuid, session=session)
+        updated_dict = user_data.dict(exclude_unset=True)
+        for field, value in updated_dict.items():
+            setattr(user_instance, field, value)
+
+        session.merge(user_instance)
+        session.commit()
+
+        payload = dict(status=True, payload=user_instance.to_dict(), message="account updated successfully")
+
+        headers = await get_headers(user_data=payload)
+        return JSONResponse(content=payload, status_code=201, headers=headers)
+
+
+@users_router.api_route(path="/user/{uuid}", methods=["GET"], include_in_schema=True)
+@authenticate_app
+async def get_user(uuid: str):
     """
         used to update a user
-    :param request:
-    :param path:
+    :param uuid:
     :return:
     """
-    users_logger.info("Get Delete USER")
+    with next(sessions) as session:
+        user_instance: Account = await Account.get_by_uuid(uuid=uuid, session=session)
+        users_logger.info("Get Delete USER")
+        # TODO Send a Login Email
 
+    payload = dict(status=True, payload=user_instance.to_dict(), message="user found")
+    headers = await get_headers(user_data=payload)
+    return JSONResponse(content=payload, status_code=201, headers=headers)
+
+
+@users_router.api_route(path="/user/{path}", methods=["DELETE"], include_in_schema=True)
+@authenticate_app
+async def delete_user(path: str):
+    """
+
+    :return:
+    """
     uuid: str = path
     with next(sessions) as session:
         user_instance: Account = await Account.get_by_uuid(uuid=uuid, session=session)
-        if request.method == "DELETE":
-            user_instance.is_deleted = True
-            session.merge(user_instance)
-            session.commit()
-            # TODO send a Goodbye Email
-            message = {'message': 'successfully deleted user'}
-            headers = await get_headers(user_data=message)
-            return JSONResponse(content=message,
-                                status_code=201,
-                                headers=headers)
-
-        elif request.method == "GET":
-            # TODO Send a Login Email
-            headers = await get_headers(user_data=user_instance.to_dict())
-            return JSONResponse(content=user_instance.to_dict(),
-                                status_code=201,
-                                headers=headers)
-
-    message = {'message': 'successfully deleted user'}
-    headers = await get_headers(user_data=message)
-    return JSONResponse(content={'message': 'deleted user'}, status_code=201, headers=headers)
-
+        user_instance.is_deleted = True
+        session.merge(user_instance)
+        session.commit()
+        # TODO send a Goodbye Email
+        message = {'message': 'successfully deleted user'}
+        headers = await get_headers(user_data=message)
+        return JSONResponse(content=message,
+                            status_code=201,
+                            headers=headers)
