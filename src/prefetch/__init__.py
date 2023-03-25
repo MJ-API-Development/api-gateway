70b1ce97cc16459e3890b87f29ee9c0905a40372
freelancing-solutions <mobiusndou@gmail.com>
2023-03-25 15:56:17 +0200

    re-enabled pre fetching

--- src/prefetch/__init__.py
+++ src/prefetch/__init__.py
@@ -9,7 +9,7 @@ async def prefetch_endpoints() -> int:
     """will fetch the endpoints causing the endpoints to be cached"""
     urls = await build_dynamic_urls()
     # will wait for a maximum of 30 seconds for a response
-    responses = await asyncio.gather(*[requester(_url, timeout=3) for _url in urls])
+    responses = await asyncio.gather(*[requester(_url, timeout=25) for _url in urls])
 
     for response, url in zip(responses, urls):
         if response and response.get("status", False):
