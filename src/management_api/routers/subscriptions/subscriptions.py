398a69dc24199038607866837f3a2d17f017a6df
freelancing-solutions <mobiusndou@gmail.com>
2023-03-29 22:25:19 +0200

    added an endpoint to fetch all the plans

--- src/management_api/routers/subscriptions/subscriptions.py
+++ src/management_api/routers/subscriptions/subscriptions.py
@@ -242,9 +242,10 @@ async def get_all_plans(request: Request):
     """
     with next(sessions) as session:
         plan_instance_list = await Plans.get_all_plans(session=session)
+        sub_logger.info(f"GET ALL PLANS : {plan_instance_list}")
         _payload = [plan.to_dict() for plan in plan_instance_list] if plan_instance_list else []
 
-    payload = dict(status=True, payload=_payload, message='Successfully retrieved plan')
-    _headers = await get_headers(user_data=payload)
+        payload = dict(status=True, payload=_payload, message='Successfully retrieved plan')
+        _headers = await get_headers(user_data=payload)
 
     return JSONResponse(content=payload, status_code=200, headers=_headers)
