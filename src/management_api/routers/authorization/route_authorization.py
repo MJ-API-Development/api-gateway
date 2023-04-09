fa0e1717f34f20845fbce2bd6065b731b3ab18e0
freelancing-solutions <mobiusndou@gmail.com>
2023-04-09 17:34:47 +0200

    updated user authorization

--- src/management_api/routers/authorization/route_authorization.py
+++ src/management_api/routers/authorization/route_authorization.py
@@ -60,16 +60,10 @@ async def check_authorization(uuid: str | None, path: str, method: str) -> bool:
     auth_logger.info(f"User is found for authorization: user: {user.uuid}")
 
     routes = ALLOWED_ROUTES["admin"] if user.is_admin else ALLOWED_ROUTES["regular"]
-    method = method.capitalize()
+    method = method.upper()
     # Check if the requested path matches any of the regular expressions in the dictionary
     for route, methods in routes.items():
-        auth_logger.info(f"Matching route : {route} - {path} with method : {method} to path: {methods}")
-        if re.match(route, path):
-            auth_logger.info("path matched")
-        if method in methods:
-            auth_logger.info("method matched")
         if re.match(route, path) and method in methods:
-            auth_logger.info(f"User is Authorized")
             return True
 
     auth_logger.info(f"User is Not Authorized")
