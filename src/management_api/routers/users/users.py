e5d8ce90eec956771f441f801f3e4dadb0514ead
freelancing-solutions <mobiusndou@gmail.com>
2023-04-09 18:03:50 +0200

    updated user authorization

--- src/management_api/routers/users/users.py
+++ src/management_api/routers/users/users.py
@@ -36,7 +36,6 @@ async def create_user(user_data: AccountCreate, request: Request) -> UserRespons
 
         if user_instance is None:
             user_dict = user_data.dict()
-            user_dict.update(uuid=create_id(UUID_LEN))
             user_instance = Account(**user_dict)
             users_logger.info(f"created user with the following user data: {user_instance.to_dict()}")
             session.add(user_instance)
