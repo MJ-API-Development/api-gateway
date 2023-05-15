6d79ff3cc8157f9a8a8f21d5130f94747dbc4d97
freelancing-solutions <mobiusndou@gmail.com>
2023-05-15 11:54:19 +0200

    code refactored

--- src/main/main.py
+++ src/main/main.py
@@ -393,13 +393,13 @@ async def startup_event():
         """will check if redis is making too many errors then switch redis off if it's the case"""
         while True:
             await redis_cache.redis_errors.check_error_threshold()
-            await asyncio.sleep(delay=60*10)
+            await asyncio.sleep(delay=60*60*10)
 
     async def monitor_servers():
         """will prioritize servers which are responsive and also available"""
         while True:
             await remote_servers.sort_api_servers_by_health()
-            await asyncio.sleep(delay=60 * 90)
+            await asyncio.sleep(delay=60 * 60 * 30)
 
     asyncio.create_task(setup_cf_firewall())
     asyncio.create_task(backup_cf_firewall_data())
